dae95fc6e6cc5fd40721b5f83151a07b58c24825
Enrique Palacios <epalaciosc@pixisproti.cl>
2021-04-26 15:27:57 -0400

    Refactorización a proyecto bajo arquitectura DAL-DTO-BLL-UI. Creación de nueva librería para uso de DAL y DTO

---
+++ SocketAppDAL/DAL/MessageDAL.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketAppDAL.DAL
+{
+    /*
+     * Quiero que cuando se envíen mensajes a través de mi DTO, los mensajes sean capturados
+     * y guardados en un archivo llamado "messages_log.txt"
+     * 
+     */
+
+    /*
+     * PATRÓN SIGLETON: Para crear una sola instancia de objetos
+     * MessagesDAL mensajesDAL = MessagesDAL.GetInstance(); -> Crea objeto referencia 1
+     * MessagesDAL mensajesDAL2 = MessagesDAL.GetInstance(); -> Se adhiere al objeto anterior referencia 1, sin crear nuevos
+     * 
+     */
+
+    public class MessageDAL
+    {
+        // Obtener ruta completa de carpeta donde está ejecutandose el .exe de la app
+        // (p.ej.: C:\Users\enriq\source\repos\SocketApp1\SocketAppDal\bin\Debug\ )
+        private string archivo = Directory.GetCurrentDirectory() + "/messages_log.txt";
+        // Ej archivo quedará en
+        // C:\Users\enriq\source\repos\SocketApp1\SocketAppDal\bin\Debug\messages_log.txt
+        private static MessageDAL instancia;
+        private MessageDAL() { } // No se puede crear a través de un MessageDAL algo = new MessageDAL();
+
+        // Método estático que reemplazará al constructor: Su función es verificar que
+        // exista una sola instancia y devolverla. O en caso de que no exista, crear una 
+        // única instancia y devolverla
+        public static MessageDAL GetInstance()
+        {
+            if(instancia == null) // No existe el objeto creado anteriormente
+            {
+                instancia = new MessageDAL(); // Constructor es privado
+            }
+
+            return instancia;
+        }
+        
+
+        // ToDo: Diseñar e implementar los métodos:
+        // * Agregar mensaje al archivo de texto
+        // * Obtener los mensajes del archivo de texto (todos)
+    }
+}
---
+++ SocketAppDAL/DTO/Message.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketAppDAL.DTO
+{
+    // En otra parte del proyecto:
+    // Message mensaje = new Message() {
+    //      Tipo = "Mensaje",
+    //      Remitente = "Servidor",
+    //      Texto = "Oli"
+    //  }
+
+    // mensaje.toString() -> (SIN CAMBIAR EL MÉTODO TO STRING) -> CONSOLE OUTPUT: [object]
+    // mensaje.toString() -> (PERSONALIZANDO MÉTODO TO STRING) -> CONSOLE OUTPUT: Mensaje;Servidor;Oli
+    public class Message
+    {
+        private string tipo;
+        private string remitente;
+        private string texto;
+
+        public string Tipo { get => tipo; set => tipo = value; }
+        public string Remitente { get => remitente; set => remitente = value; }
+        public string Texto { get => texto; set => texto = value; }
+
+        public override string ToString()
+        {
+            //return Tipo + ";" + Remitente + ";" + Texto;
+            return $"{Tipo};{Remitente};{Texto}";
+        }
+
+    }
+}
+
