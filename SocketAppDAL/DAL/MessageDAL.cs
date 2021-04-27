aa3578fc3bb1219705a37a1106b9782fe6b34441
Enrique Palacios <epalaciosc@pixisproti.cl>
2021-04-27 14:50:04 -0400

    Finalizado el DAL de mensajes

--- SocketAppDAL/DAL/MessageDAL.cs
+++ SocketAppDAL/DAL/MessageDAL.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SocketAppDAL.DTO;
 
 namespace SocketAppDAL.DAL
 {
@@ -42,10 +43,71 @@ namespace SocketAppDAL.DAL
 
             return instancia;
         }
-        
 
-        // ToDo: Diseñar e implementar los métodos:
-        // * Agregar mensaje al archivo de texto
+        // Agregar mensajes al archivo de texto
+        public void Add(Message message)
+        {
+            // Streams (Writer & Reader)
+            //StreamWriter writer = new StreamWriter(archivo); // Vacía el archivo y escribe el mensaje
+
+            /* Con destructor */
+            /*
+            StreamWriter writer = new StreamWriter(archivo, true); // Abre el archivo y anexa el mensaje
+
+            writer.WriteLine(message);
+            writer.Flush();
+
+            writer.Dispose(); // Llamar a Destructor (en cualquier lenguaje) */
+            // DESTRUCTOR SE LLAMA PARA LIBERAR EL OBJETO DE MEMORIA Y TAMBIÉN LIBERAR
+            // EL ARCHIVO "message_log.txt"
+
+            /* Con técnica USING de C# */
+            // Using: Instanciar un objeto y cuando se termine de ejecutar
+            // todo su bloque de código, llama automaticamente al destructor (bórralo apenas termina)
+            using(StreamWriter writer = new StreamWriter(archivo, true))
+            {
+                writer.WriteLine(message);
+                writer.Flush();
+            }
+
+        }
+
         // * Obtener los mensajes del archivo de texto (todos)
+        public List<Message> GetAll()
+        {
+            // Creamos una lista vacía para guardar los valores
+            List<Message> mensajes = new List<Message>();
+            
+            // 1. Crear un lector que tenga duración definida
+            using (StreamReader reader = new StreamReader(archivo))
+            {
+                string lineaLeida = null; // variable que guardará cada línea
+
+                do
+                {
+                    lineaLeida = reader.ReadLine(); // Leer línea, guardar el valor y pasar a la siguiente línea
+                    if(lineaLeida != null)
+                    {
+                        // Construir el objeto message a partir de una línea de texto
+                        // Trim = Eliminar espacios vacíos al comienzo y al final de un string
+                        string[] textos = lineaLeida.Trim().Split(';'); // Guardar elementos separados por ;
+                        Message mensaje = new Message()
+                        {
+                            Tipo = textos[0],
+                            Remitente = textos[1],
+                            Texto = textos[2]
+                        };
+                        mensajes.Add(mensaje);
+                        /* RETRO 
+                        mensaje.Tipo = textos[0];
+                        mensaje.Remitente = textos[1];
+                        mensaje.Texto = textos[2]; */
+                    }
+
+                } while (lineaLeida != null);
+            }
+
+            return mensajes;            
+        }
     }
 }
